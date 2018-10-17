b52b17f9c40e0b6d9511d3344189e20292c9bb43
Unknown <heianhourai@gmail.com>
2018-10-17 13:25:40 -0400

    Added movement for player character.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static Player pc;
+
+    //Controls how fast the player moves.
+    public float mvtSpd;
+
+    public void Awake()
+    {
+        if (pc == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            pc = this;
+        }
+        else if (pc != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        movePC();
 	}
+
+    //As the name says, this function will move the player character around. Easy.
+    public void movePC()
+    {
+        bool tryUp = Input.GetKey(KeyCode.W);
+        bool tryDown = Input.GetKey(KeyCode.S);
+        bool tryLeft = Input.GetKey(KeyCode.A);
+        bool tryRight = Input.GetKey(KeyCode.D);
+
+        Vector2 mvtDir = Vector2.zero;
+        if (tryUp)
+        {
+            mvtDir += Vector2.up;
+        }
+        if (tryDown)
+        {
+            mvtDir += Vector2.down;
+        }
+        if (tryLeft)
+        {
+            mvtDir += Vector2.left;
+        }
+        if (tryRight)
+        {
+            mvtDir += Vector2.right;
+        }
+        mvtDir.Normalize();
+        transform.position += new Vector3(mvtDir.x * Time.deltaTime * mvtSpd, mvtDir.y * Time.deltaTime * mvtSpd);
+    }
 }
