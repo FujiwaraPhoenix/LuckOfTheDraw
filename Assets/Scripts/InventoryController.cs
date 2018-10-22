8bfaec14d10152612871e6c819d1cbd864038265
Unknown <ianouellette47@gmail.com>
2018-10-22 15:03:26 -0400

    Added Resource and InventoryController Scripts
    Added scripts for the resources and inventory management. Also added the resource tag.

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour { //Resources must have the "Resource" tag, and the ui canvas should have three images for the inventory ui
+    public static InventoryController ic;
+
+    //Copies of the resources that were picked up and slotted in, used to instantiate when slotted out
+    GameObject gunResource;
+    GameObject shotResource;
+    GameObject effectResource;
+
+    //Whether or not the respective slot is filled
+    bool haveGun = false;
+    bool haveShot = false;
+    bool haveEffect = false;
+
+    //The numbers used for the inventory slots
+    int gunIndex = 0;
+    int shotIndex = 0;
+    int effectIndex = 0;
+
+    //The images of the resources in the respective slots
+    public Image gunImage;
+    public Image shotImage;
+    public Image effectImage;
+
+    //Workarounds for using input.getKey in a trigger method
+    bool pressOne = false;
+    bool pressTwo = false;
+    bool pressThree = false;
+
+    //Cooldown stuff
+    float cooldown = 0;
+
+    public void Awake()
+    {
+        if (ic == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            ic = this;
+        }
+        else if (ic != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            pressOne = true;
+            pressTwo = false;
+            pressThree = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            pressOne = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            pressOne = false;
+            pressTwo = true;
+            pressThree = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            pressTwo = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            pressOne = false;
+            pressTwo = false;
+            pressThree = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            pressThree = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("In a trigger");
+        if(cooldown <= 0 && collision.tag == "Resource") //If the player is over the triggerbox of a resource
+        {
+            Debug.Log("Trigger is resource");
+            //Depending on what key the player presses, take the resource and input it into a modularity slot
+            //If the player already has a resource in that slot, drop the previous resource on the ground
+            //Update the references for what resource is in the slot, the modularity instance, and the ui sprite
+            //Set the cooldown to .5 seconds
+
+            Resource r = collision.GetComponent<Resource>();
+            if(pressOne)
+            {
+                Debug.Log("Taking Action");
+                if(haveGun)
+                {
+                    Instantiate(gunResource, transform.position, Quaternion.identity);
+                }
+                gunResource = r.gameObject;
+                gunIndex = r.pickup();
+                gunImage.sprite = r.giveSprite();
+                haveGun = true;
+                cooldown = 0.5f;
+            }
+            else if (pressTwo)
+            {
+                if (haveShot)
+                {
+                    Instantiate(shotResource, transform.position, Quaternion.identity);
+                }
+                shotResource = r.gameObject;
+                shotIndex = r.pickup();
+                shotImage.sprite = r.giveSprite();
+                haveShot = true;
+                cooldown = 0.5f;
+            }
+            else if (pressThree)
+            {
+                if (haveEffect)
+                {
+                    Instantiate(effectResource, transform.position, Quaternion.identity);
+                }
+                effectResource = r.gameObject;
+                effectIndex = r.pickup();
+                effectImage.sprite = r.giveSprite();
+                haveEffect = true;
+                cooldown = 0.5f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public bool isFruit; //Whether or not the resource is a fruit, and can thus be eaten.
+
+    public int modifierNum; //The number corresponding to what the resource does when put in a slot
+
+    bool isGone = false; //Whether or not the resource has been picked up and should delete itself
+
+    Sprite self;
+    SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        self = sr.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGone)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public int pickup() //When picked up, tell the caller how it affects it the modularity and tell the resource to remove itself
+    {
+        isGone = true;
+        return modifierNum;
+    }
+
+    public Sprite giveSprite() //Give a copy of whatever sprite the resource uses
+    {
+        return self;
+    }
+
+    public void eat() //If the resource is a fruit, eat it
+    {
+        if (isFruit)
+        {
+            //Do the eat stuff. May need to update the return value
+            isGone = true;
+        }
+    }
+}
