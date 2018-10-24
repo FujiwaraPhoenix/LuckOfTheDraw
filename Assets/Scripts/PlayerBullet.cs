b294c95b4241b4375c0faee6299d3cf9c8c7bb3f
Unknown <heianhourai@gmail.com>
2018-10-24 14:00:51 -0400

    Added framework for shooting and testing thereof.

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+    public Vector3 travelDir;
+    public bool isSine = false;
+    public int aftereffect = 0;
+    public int mvtSpd;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += travelDir * mvtSpd * Time.deltaTime;
+	}
+}
