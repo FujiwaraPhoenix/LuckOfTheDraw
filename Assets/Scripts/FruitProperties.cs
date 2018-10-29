f14968c90a2bd9f152b114534ea4667e76d79d3b
Unknown <bf1081@nyu.edu>
2018-10-29 03:12:10 -0400

    Player fruit consumption
    gonna need some help with stat changes..no idea what's supposed to do what...hold spacebar to suck in fruit

--- Assets/Scripts/FruitProperties.cs
+++ Assets/Scripts/FruitProperties.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class FruitProperties : MonoBehaviour {
     public int HealthAmount;
+    public static int publicHealth;
     public int HungerAmount;
+    public static int publicHunger;
     public float SpeedPropety;
+    public static float publicSpeed;
     public int ArmorProperty;
+    public static int publicArmor;
     public float Regenproperty;
+    public static float publicRegen;
 
 	// Use this for initialization
 	void Start () {
+
+    
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
-	}
+        publicHealth = HealthAmount;
+        publicHunger = HungerAmount;
+        publicArmor = ArmorProperty;
+        publicSpeed = SpeedPropety;
+        publicRegen = Regenproperty;
+
+
+    }
 }
