f30db44f808713890800d758cb8d0f8493ada9a1
Unknown <heianhourai@gmail.com>
2018-11-03 13:04:59 -0400

    Edited UI placement; shot spread and territorial active; win text added.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
 
     //Makes sure that there is only one Controller in the scene at any given point in time.
     public static Controller Instance;
+    public Text winText;
+    public bool bossBeaten;
 
     //Do not touch this!
     public void Awake()
@@ -28,6 +31,13 @@ public class Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (bossBeaten)
+        {
+            winText.gameObject.SetActive(true);
+        }
+        else
+        {
+            winText.gameObject.SetActive(false);
+        }
 	}
 }
