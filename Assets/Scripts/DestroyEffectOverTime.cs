e051c4ec00e44d932e81d43c60ac1df173355858
Nick <neissanicholas@gmail.com>
2018-11-07 13:42:42 -0500

    Created effect destruction script, created eating effect

---
+++ Assets/Scripts/DestroyEffectOverTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEffectOverTime : MonoBehaviour {
+    float timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 1f * Time.deltaTime;
+        if(timer > 1f)
+        {
+            Destroy(this.gameObject);
+        }	
+	}
+}
